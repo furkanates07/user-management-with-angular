536a05c6c0f923e5a0502a15cf10d8e75162c3dc
Furkan Ateş <furkanates4@posta.mu.edu.tr>
2025-05-15 16:17:51 +0300

    feat: create emits for search and sort by columns

--- src/app/components/ui/user/user-table/user-table.component.ts
+++ src/app/components/ui/user/user-table/user-table.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { SortType } from '../../../../enums/sort.enum';
 import { User } from '../../../../models/user.model';
 import { MaskPasswordPipe } from '../../../../shared/pipes/mask-password.pipe';
 import { DeleteDialogComponent } from '../user-delete-dialog/user-delete-dialog.component';
@@ -9,12 +11,48 @@ import { UserInfoDialogComponent } from '../user-info-dialog/user-info-dialog.co
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
-  imports: [CommonModule, MaskPasswordPipe],
+  imports: [CommonModule, MaskPasswordPipe, FormsModule],
   styleUrls: ['./user-table.component.css'],
 })
 export class UserTableComponent {
   @Input() users: User[] = [];
   @Output() delete = new EventEmitter<number>();
+
+  tableSearchQuery = {
+    id: null as number | null,
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    phone: '',
+    isVerified: null as boolean | null,
+  };
+
+  @Output() searchQueryChange = new EventEmitter<{
+    id: number | null;
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+    phone: string;
+    isVerified: boolean | null;
+  }>();
+
+  sortDirections: Record<keyof User, SortType> = {
+    id: SortType.DEF,
+    first_name: SortType.DEF,
+    last_name: SortType.DEF,
+    username: SortType.DEF,
+    email: SortType.DEF,
+    phone: SortType.DEF,
+    password: SortType.DEF,
+    isVerified: SortType.DEF,
+  };
+
+  @Output() sortTypeChange = new EventEmitter<{
+    field: keyof User;
+    direction: SortType;
+  }>();
 
   constructor(public dialog: MatDialog) {}
 
@@ -35,5 +73,43 @@ export class UserTableComponent {
     });
 
     dialogRef.afterClosed().subscribe(() => {});
+  }
+
+  filterUsersFromTable() {
+    this.searchQueryChange.emit(this.tableSearchQuery);
+  }
+
+  onIdChange(value: string) {
+    const parsed = parseInt(value, 10);
+    this.tableSearchQuery.id = isNaN(parsed) ? null : parsed;
+    this.filterUsersFromTable();
+  }
+
+  resetTableSearchQuery() {
+    this.tableSearchQuery = {
+      id: null,
+      first_name: '',
+      last_name: '',
+      username: '',
+      email: '',
+      phone: '',
+      isVerified: null,
+    };
+    this.searchQueryChange.emit(this.tableSearchQuery);
+  }
+
+  cycleSort(field: keyof User) {
+    const current = this.sortDirections[field];
+
+    const next =
+      current === SortType.DEF
+        ? SortType.ASC
+        : current === SortType.ASC
+        ? SortType.DESC
+        : SortType.DEF;
+
+    this.sortDirections[field] = next;
+
+    this.sortTypeChange.emit({ field, direction: next });
   }
 }
