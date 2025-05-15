49037b56f9122e4c328d32a5aeb3e8ee9dcd0b0b
Furkan Ateş <furkanates4@posta.mu.edu.tr>
2025-05-15 16:19:16 +0300

    feat: define emits and their funcs

--- src/app/pages/user/user-list/user-list.component.ts
+++ src/app/pages/user/user-list/user-list.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { UserTableHeaderComponent } from '../../../components/ui/user/user-table-header/user-table-header.component';
 import { UserTableComponent } from '../../../components/ui/user/user-table/user-table.component';
 import { UserService } from '../../../core/services/user.service';
+import { SortType } from '../../../enums/sort.enum';
 import { User } from '../../../models/user.model';
 
 @Component({
@@ -14,7 +15,26 @@ import { User } from '../../../models/user.model';
 export class UserListComponent implements OnInit {
   users: User[] = [];
   searchQuery: string = '';
+  tableSearchQuery = {
+    id: null as number | null,
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    phone: '',
+    isVerified: null as boolean | null,
+  };
   filteredUsers: User[] = [];
+  sortDirections: Record<keyof User, 'asc' | 'desc' | 'def'> = {
+    id: 'def',
+    first_name: 'def',
+    last_name: 'def',
+    username: 'def',
+    email: 'def',
+    phone: 'def',
+    password: 'def',
+    isVerified: 'def',
+  };
 
   constructor(private userService: UserService) {}
 
@@ -28,6 +48,7 @@ export class UserListComponent implements OnInit {
   ngOnChanges(changes: SimpleChanges) {
     if (changes) {
       this.filterUsers();
+      this.filterUsersFromTable();
     }
   }
 
@@ -50,6 +71,71 @@ export class UserListComponent implements OnInit {
     } else {
       this.filteredUsers = [...this.users];
     }
+  }
+
+  filterUsersFromTable() {
+    this.filteredUsers = this.users.filter((user) => {
+      return (
+        (this.tableSearchQuery.id === null ||
+          String(user.id).includes(String(this.tableSearchQuery.id))) &&
+        (!this.tableSearchQuery.first_name ||
+          user.first_name
+            .toLowerCase()
+            .includes(this.tableSearchQuery.first_name.toLowerCase())) &&
+        (!this.tableSearchQuery.last_name ||
+          user.last_name
+            .toLowerCase()
+            .includes(this.tableSearchQuery.last_name.toLowerCase())) &&
+        (!this.tableSearchQuery.username ||
+          user.username
+            .toLowerCase()
+            .includes(this.tableSearchQuery.username.toLowerCase())) &&
+        (!this.tableSearchQuery.email ||
+          user.email
+            .toLowerCase()
+            .includes(this.tableSearchQuery.email.toLowerCase())) &&
+        (!this.tableSearchQuery.phone ||
+          user.phone
+            .toLowerCase()
+            .includes(this.tableSearchQuery.phone.toLowerCase())) &&
+        (this.tableSearchQuery.isVerified === null ||
+          user.isVerified === this.tableSearchQuery.isVerified)
+      );
+    });
+  }
+
+  onSortTypeChange(event: { field: keyof User; direction: SortType }) {
+    this.sortDirections[event.field] = event.direction;
+    this.sortUsers(event.field, event.direction);
+  }
+
+  sortUsers(field: keyof User, direction: SortType) {
+    console.log('Sorting users by', field, 'in', direction, 'order');
+    if (direction === SortType.DEF) {
+      this.filteredUsers = [...this.users];
+      this.filterUsers();
+      this.filterUsersFromTable();
+      return;
+    }
+
+    this.filteredUsers.sort((a, b) => {
+      const aValue = a[field];
+      const bValue = b[field];
+
+      if (aValue == null || bValue == null) return 0;
+
+      if (typeof aValue === 'string' || typeof bValue === 'string') {
+        return direction === SortType.ASC
+          ? String(aValue).localeCompare(String(bValue))
+          : String(bValue).localeCompare(String(aValue));
+      }
+
+      if (typeof aValue === 'number' && typeof bValue === 'number') {
+        return direction === SortType.ASC ? aValue - bValue : bValue - aValue;
+      }
+
+      return 0;
+    });
   }
 
   deleteUser(id: number): void {
